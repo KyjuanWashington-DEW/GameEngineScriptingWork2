fdfc6a25c1e999590b28dcb44571331307f18ffd
KyjuanDEW <k1washingtonofficial@gmail.com>
2024-04-19 21:44:17 -0500

    Implemented Code

--- Assets/makeup assignment 2/Scripts/Player2.cs
+++ Assets/makeup assignment 2/Scripts/Player2.cs
@@ -5,6 +5,7 @@ public class Player2 : MonoBehaviour
     private ElevatorDetection elevatorDetection; 
     private ElevatorButton currentButton; 
     public float moveSpeed = 5f;
+    public float rotationSpeed = 95f;
 
     void Start()
     {
@@ -38,12 +39,18 @@ public class Player2 : MonoBehaviour
         //TODO: This function should be called from your player script
         //      You will need to create a reference to ElevatorDetection in your player script and assign it as a reference
 
-   
-       // add if (input.getkeydown code here delete public
 
-    }
-   
+        // add if (input.getkeydown code here delete public
 
         
-      
+
+        // Calculate rotation
+        Vector3 rotation = new Vector3(verticalInput, horizontalInput, 0) * rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotation);
+
+    }
+
+
+
+
 }
